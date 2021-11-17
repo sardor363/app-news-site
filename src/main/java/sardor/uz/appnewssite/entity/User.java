b1a4fbf4d63f7a80f18cc7eed7c45051b54e784f
sardor <sardorbekodiljonov363@gmail.com>
2021-11-17 10:39:40 +0500

    User

--- src/main/java/sardor/uz/appnewssite/entity/User.java
+++ src/main/java/sardor/uz/appnewssite/entity/User.java
@@ -2,13 +2,25 @@ package sardor.uz.appnewssite.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import sardor.uz.appnewssite.entity.template.AbsEntity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 @Entity(name = "users")
 @AllArgsConstructor
 @NoArgsConstructor
-public class User {
+public class User extends AbsEntity {
+    @Column(nullable = false)
+    private String fullName;
+
+    @Column(nullable = false,unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
 }
--- src/main/java/sardor/uz/appnewssite/entity/template/AbsEntity.java
+++ src/main/java/sardor/uz/appnewssite/entity/template/AbsEntity.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
 import sardor.uz.appnewssite.entity.User;
 
 import javax.persistence.*;
@@ -28,6 +29,10 @@ public abstract class AbsEntity {
     @JoinColumn(updatable = false)
     @CreatedBy
     @ManyToOne(fetch = FetchType.LAZY)
-    private User user;
+    private User createdBy;
+
+    @LastModifiedBy
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User updatedBy;
 
 }
