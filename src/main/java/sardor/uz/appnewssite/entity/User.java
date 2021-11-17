39bed2b8f5ef0cf43d2db4f21552db5888444147
sardor <sardorbekodiljonov363@gmail.com>
2021-11-17 10:10:37 +0300

    Initial commit

---
+++ src/main/java/sardor/uz/appnewssite/entity/User.java
@@ -0,0 +1,14 @@
+package sardor.uz.appnewssite.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Data
+@Entity(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+}
---
+++ src/main/java/sardor/uz/appnewssite/entity/template/AbsEntity.java
@@ -0,0 +1,33 @@
+package sardor.uz.appnewssite.entity.template;
+
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedBy;
+import sardor.uz.appnewssite.entity.User;
+
+import javax.persistence.*;
+import java.sql.Time;
+import java.sql.Timestamp;
+
+@MappedSuperclass
+@Data
+public abstract class AbsEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(updatable = false, nullable = false)
+    @CreationTimestamp
+    private Timestamp createdAt;
+
+    @Column(nullable = false)
+    @UpdateTimestamp
+    private Timestamp updatedAt;
+
+    @JoinColumn(updatable = false)
+    @CreatedBy
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+}
