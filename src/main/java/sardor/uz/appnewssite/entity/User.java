b191a7ae8eee9cf09cd1cf6d6a37b3f9dedc5fc9
sardor <sardorbekodiljonov363@gmail.com>
2021-11-17 15:53:42 +0500

    User

--- src/main/java/sardor/uz/appnewssite/entity/User.java
+++ src/main/java/sardor/uz/appnewssite/entity/User.java
@@ -4,23 +4,54 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import sardor.uz.appnewssite.entity.enums.Permission;
 import sardor.uz.appnewssite.entity.template.AbsEntity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToOne;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
 @Entity(name = "users")
 @AllArgsConstructor
 @NoArgsConstructor
-public class User extends AbsEntity {
+public class User extends AbsEntity implements UserDetails {
     @Column(nullable = false)
     private String fullName;
 
-    @Column(nullable = false,unique = true)
+    @Column(nullable = false, unique = true)
     private String username;
 
     @Column(nullable = false)
     private String password;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    private Role role;
+
+    private boolean enabled;
+    private boolean accountNonExpired = true;
+    private boolean accountNonLocked = true;
+    private boolean credentialsNonExpired = true;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<Permission> permissionList = this.role.getPermissionList();
+        List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+        for (Permission permission : permissionList) {
+            grantedAuthorities.add(new GrantedAuthority() {
+                @Override
+                public String getAuthority() {
+                    return permission.name();
+                }
+            });
+        }
+        return grantedAuthorities;
+    }
 }
